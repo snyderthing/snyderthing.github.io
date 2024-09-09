de309fa432190b4467f10e41bef8cc23bab81f3b
snyderthing <gmsladen@gmail.com>
2024-09-09 12:56:28 -0700

    Added The Math Table

---
+++ TheMathTable/mt_calc.js
@@ -0,0 +1,72 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5, CSS3 and JavaScript 6th Edition
+   Tutorial 11
+   Case Problem 2
+
+   Author: 
+   Date:   
+   
+   Filename: mt_calc.js
+	
+   Functions List:
+
+   init()
+      Initializes the contents of the web page and sets up the
+      event handlers
+      
+   buttonClick(e)
+      Adds functions to the buttons clicked within the calcutlor
+      
+   calcKeys(e)
+      Adds functions to key pressed within the calculator window 
+      
+   eraseChar(textStr)
+      Erases the last character from the text string, textStr
+      
+   evalEq(textStr, decimals) 
+      Evaluates the equation in textStr, returning a value to the number of decimals specified by the decimals parameter
+
+   lastEq(textStr) 
+      Returns the previous expression from the list of expressions in the textStr parameter
+
+*/
+
+
+
+//Function to define what each button does
+
+
+//Function to specify behavior of keyboard keys
+
+
+/* ===================================================================== */
+
+// Run the init() function when the page is loaded by the browser
+window.onload = init;
+
+function init() {
+    // Declare the calcButtons variable containing the collection of page 
+    // elements belonging to the calcButton class
+    var calcButtons;
+    window.alert("Hello \
+world!");
+}
+
+function eraseChar(textStr) {
+   return textStr.substr(0, textStr.length - 1);
+}
+
+function evalEq(textStr, decimals) {
+   var lines = textStr.split(/\r?\n/);
+   var lastLine = lines[lines.length-1];
+   var eqValue = eval(lastLine);
+   return eqValue.toFixed(decimals);
+}
+
+function lastEq(textStr) {
+   var lines = textStr.split(/\r?\n/);
+   var lastExp = lines[lines.length-2];
+   return lastExp.substr(0, lastExp.indexOf("=")).trim();
+}
