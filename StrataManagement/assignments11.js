62978a5fe4184b875a484d2e4678d24ccd682525
snyderthing <gmsladen@gmail.com>
2024-12-13 16:11:21 -0800

    Started assignment 11

---
+++ StrataManagement/assignments11.js
@@ -0,0 +1,246 @@
+const questions = [
+    {
+        // Question 1
+        question: `Strata Corporation ABC (“ABC”) contains 120 strata lots, 80 of which are apartment-style units located in a single high-rise tower, and 40 of which are townhouse-style units located in multiple separate buildings on the strata property. The townhouse owners do not have access to the high-rise tower. There is a separate amenities building on the property to which all owners have access. The townhouse owners are unhappy about having to contribute to expenses that they feel relate solely to the high-rise tower. Therefore, the townhouse owners would like to amend ABC’s bylaws to create two sections: an apartment section and a townhouse section. At the following AGM, which all of ABC’s owners attend, resolutions to amend the bylaws to create the two sections were put to a vote. Brenda, the strata manager, accurately counts and records the following results:<br><br>
+        <table border='1' cellspacing='0' cellpadding='0' align='center'>
+        <tbody>
+        <tr>
+        <td style='width: 104.3pt;' valign='top'><p>&nbsp;</p></td>
+        <td style='width: 105.1pt; text-align: center;' valign='top'>
+        <p><strong>Apartment Owners</strong></p>
+        </td>
+        <td style='width: 114.6pt; text-align: center;' valign='top'>
+        <p><strong>Townhouse Owners</strong></p>
+        </td>
+        <td style='width: 103.5pt; text-align: center;' valign='top'>
+        <p><strong>Total</strong></p>
+        </td>
+        </tr>
+
+        <tr>
+                <td style="width: 104.3pt; text-align: center;" valign="top">
+        <p>Votes <strong><u>FOR</u></strong> the creation of the sections</p>
+        </td>
+        <td style="width: 105.1pt; text-align: center;" valign="top">
+        <p>55</p>
+        </td>
+        <td style="width: 114.6pt; text-align: center;" valign="top">
+        <p>37</p>
+        </td>
+        <td style="width: 103.5pt; text-align: center;" valign="top">
+        <p>92</p>
+        </td>
+        </tr>
+
+        <tr>
+        <td style="width: 104.3pt; text-align: center;" valign="top">
+        <p>Votes <strong><u>AGAINST</u></strong> the creation of the sections</p>
+        </td>
+        <td style="width: 105.1pt; text-align: center;" valign="top">
+        <p>25</p>
+        </td>
+        <td style="width: 114.6pt; text-align: center;" valign="top">
+        <p>3</p>
+        </td>
+        <td style="width: 103.5pt; text-align: center;" valign="top">
+        <p>28</p>
+        </td>
+        </tr>
+
+        <tr>
+        <td style="width: 104.3pt; text-align: center;" valign="top">
+        <p>TOTAL</p>
+        </td>
+        <td style="width: 105.1pt; text-align: center;" valign="top">
+        <p>80</p>
+        </td>
+        <td style="width: 114.6pt; text-align: center;" valign="top">
+        <p>40</p>
+        </td>
+        <td style="width: 103.5pt; text-align: center;" valign="top">
+        <p>120</p>
+        </td>
+        </tr>
+        </tbody>
+        </table>
+        <br>
+        <p>Assuming that ABC did NOT have any existing sections prior to this vote, Brenda would be correct in declaring that:</p>
+        `,
+        choices: ["Since less than ¾ of the apartment owners voted in favour of the resolutions, neither of the sections has been created.",
+        "Since at least ¾ of the total owners voted in favour of the resolutions, the two sections have been created.",
+        "Since at least ¾ of the townhouse owners and at least ¾ of the total owners voted in favour of the resolutions, but less than ¾ of the apartment owners voted in favour of the resolutions, only the townhouse section has been created.",
+        "Since at least ½ of the apartment owners, the townhouse owners, and the total owners voted in favour of the resolutions, the two sections have been created."],
+        correct: 2
+    },
+    {
+        // Question 2
+        question: "A strata corporation has just successfully created a townhouse section. Which of the following is TRUE about this newly formed section?",
+        choices: ["The section must pass its own set of bylaws.",
+        "The section must elect an executive.",
+        "The strata corporation and section may no t use the same strata management company.",
+        "All of the above"],
+        correct: 0
+    },
+    {
+        // Question 3
+        question: "",
+        choices: ["",
+        "",
+        "",
+        ""],
+        correct: 0
+    },
+    {
+        // Question 4
+        question: "",
+        choices: ["",
+        "",
+        "",
+        ""],
+        correct: 0
+    },
+    {
+        // Question 5
+        question: "",
+        choices: ["",
+        "",
+        "",
+        ""],
+        correct: 0
+    },
+    {
+        // Question 6
+        question: "",
+        choices: ["",
+        "",
+        "",
+        ""],
+        correct: 0
+    },
+    {
+        // Question 7
+        question: "",
+        choices: ["",
+        "",
+        "",
+        ""],
+        correct: 0
+    },
+    {
+        // Question 8
+        question: "",
+        choices: ["",
+        "",
+        "",
+        ""],
+        correct: 0
+    },
+    {
+        // Question 9
+        question: "",
+        choices: ["",
+        "",
+        "",
+        ""],
+        correct: 0
+    },
+    {
+        // Question 10
+        question: "",
+        choices: ["",
+        "",
+        "",
+        ""],
+        correct: 0
+    },
+    {
+        // Question 11
+        question: "",
+        choices: ["",
+        "",
+        "",
+        ""],
+        correct: 0
+    },
+    {
+        // Question 12
+        question: "",
+        choices: ["",
+        "",
+        "",
+        ""],
+        correct: 0
+    },
+    {
+        // Question 13
+        question: "",
+        choices: ["",
+        "",
+        "",
+        ""],
+        correct: 0
+    },
+    {
+        // Question 14
+        question: "",
+        choices: ["",
+        "",
+        "",
+        ""],
+        correct: 0
+    },
+    {
+        // Question 15
+        question: "",
+        choices: ["",
+        "",
+        "",
+        ""],
+        correct: 0
+    },
+    {
+        // Question 16
+        question: "",
+        choices: ["",
+        "",
+        "",
+        ""],
+        correct: 0
+    },
+    {
+        // Question 17
+        question: "",
+        choices: ["",
+        "",
+        "",
+        ""],
+        correct: 0
+    },
+    {
+        // Question 18
+        question: "",
+        choices: ["",
+        "",
+        "",
+        ""],
+        correct: 0
+    },
+    {
+        // Question 19
+        question: "",
+        choices: ["",
+        "",
+        "",
+        ""],
+        correct: 0
+    },
+    {
+        // Question 120
+        question: "",
+        choices: ["",
+        "",
+        "",
+        ""],
+        correct: 0
+    }
+];
---
+++ StrataManagement/assignments99.js
@@ -0,0 +1,182 @@
+const questions = [
+    {
+        // Question 1
+        question: "",
+        choices: ["",
+        "",
+        "",
+        ""],
+        correct: 0
+    },
+    {
+        // Question 2
+        question: "",
+        choices: ["",
+        "",
+        "",
+        ""],
+        correct: 0
+    },
+    {
+        // Question 3
+        question: "",
+        choices: ["",
+        "",
+        "",
+        ""],
+        correct: 0
+    },
+    {
+        // Question 4
+        question: "",
+        choices: ["",
+        "",
+        "",
+        ""],
+        correct: 0
+    },
+    {
+        // Question 5
+        question: "",
+        choices: ["",
+        "",
+        "",
+        ""],
+        correct: 0
+    },
+    {
+        // Question 6
+        question: "",
+        choices: ["",
+        "",
+        "",
+        ""],
+        correct: 0
+    },
+    {
+        // Question 7
+        question: "",
+        choices: ["",
+        "",
+        "",
+        ""],
+        correct: 0
+    },
+    {
+        // Question 8
+        question: "",
+        choices: ["",
+        "",
+        "",
+        ""],
+        correct: 0
+    },
+    {
+        // Question 9
+        question: "",
+        choices: ["",
+        "",
+        "",
+        ""],
+        correct: 0
+    },
+    {
+        // Question 10
+        question: "",
+        choices: ["",
+        "",
+        "",
+        ""],
+        correct: 0
+    },
+    {
+        // Question 11
+        question: "",
+        choices: ["",
+        "",
+        "",
+        ""],
+        correct: 0
+    },
+    {
+        // Question 12
+        question: "",
+        choices: ["",
+        "",
+        "",
+        ""],
+        correct: 0
+    },
+    {
+        // Question 13
+        question: "",
+        choices: ["",
+        "",
+        "",
+        ""],
+        correct: 0
+    },
+    {
+        // Question 14
+        question: "",
+        choices: ["",
+        "",
+        "",
+        ""],
+        correct: 0
+    },
+    {
+        // Question 15
+        question: "",
+        choices: ["",
+        "",
+        "",
+        ""],
+        correct: 0
+    },
+    {
+        // Question 16
+        question: "",
+        choices: ["",
+        "",
+        "",
+        ""],
+        correct: 0
+    },
+    {
+        // Question 17
+        question: "",
+        choices: ["",
+        "",
+        "",
+        ""],
+        correct: 0
+    },
+    {
+        // Question 18
+        question: "",
+        choices: ["",
+        "",
+        "",
+        ""],
+        correct: 0
+    },
+    {
+        // Question 19
+        question: "",
+        choices: ["",
+        "",
+        "",
+        ""],
+        correct: 0
+    },
+    {
+        // Question 120
+        question: "",
+        choices: ["",
+        "",
+        "",
+        ""],
+        correct: 0
+    }
+];
