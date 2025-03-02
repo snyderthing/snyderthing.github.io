6ffb8fda113478544867b8ffe1284cec89990130
snyderthing <gmsladen@gmail.com>
2025-03-01 18:03:33 -0800

    JavaScript practice

---
+++ JavaScriptLand/app.js
@@ -0,0 +1,43 @@
+console.log("Hello from app.js!");
+/*
+let age = prompt("How old are you?");
+age = parseInt(age);
+if (age === 57) {
+    alert("Good age, man!");
+}
+*/
+
+
+/* let random = Math.random();
+if (random < 0.5) {
+    console.log("It was less than 0.5.");
+}
+
+console.log(random);
+ */
+// -----------
+/* const phrase = "lol";
+
+
+function getColor(phrase){
+    //WRITE YOUR CODE BETWEEN THIS LINE: ↓ ↓ ↓ ↓ 
+   if (phrase === "stop") {
+       console.log("red");
+   } else if (phrase === "slow"){
+       console.log("yellow");
+   } else if (phrase === "go") {
+           console.log("green");
+   } else {
+           console.log("purple");
+   }
+   
+    //AND THIS LINE ↑↑↑↑↑
+}
+
+getColor(phrase);
+ */
+
+
+
+
+console.log("Goodbye.");
