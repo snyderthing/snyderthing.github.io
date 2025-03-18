1d47428fda354c97a7ad7893b90f7f4a59f2b9c4
snyderthing <gmsladen@gmail.com>
2025-03-18 15:58:07 -0700

    Move Canvas

--- JavaScriptLand/canvas.js
+++ JavaScriptLand/canvas.js
@@ -73,3 +73,53 @@ var i = document.getElementById("myCanvas7");
 var itx = i.getContext("2d");
 var img = document.getElementById("scream");
 itx.drawImage(img, 10, 10);
+
+
+const j = document.getElementById("myCanvas8");
+const jtx = j.getContext("2d");
+
+jtx.rect(10, 10, 150, 75);
+jtx.rect(160, 75, 40, 25);
+jtx.stroke();
+jtx.fillStyle = "red";
+jtx.fillRect(162, 77, 30, 20)
+
+const k = document.getElementById("myCanvas9");
+const ktx = k.getContext("2d");
+
+ktx.strokeStyle = "blue";
+ktx.strokeRect(10, 10, 120, 60);
+
+const l = document.getElementById("myCanvas10");
+const ltx = l.getContext("2d");
+
+ltx.fillStyle = "blue";
+ltx.fillRect(10, 10, 150, 100);
+ltx.clearRect(60, 35, 50, 50);
+
+const m = document.getElementById("myCanvas11");
+const mtx = m.getContext("2d");
+
+mtx.beginPath();
+mtx.arc(95, 50, 40, 0, 1.5 * Math.PI);
+mtx.fillStyle = "red";
+mtx.fill();
+mtx.lineWidth = 4;
+mtx.strokeStyle = "blue";
+mtx.stroke();
+
+const n = document.getElementById("myCanvas12");
+const ntx = n.getContext("2d");
+
+ntx.beginPath();
+ntx.moveTo(10, 100);
+ntx.quadraticCurveTo(250, 170, 230, 20);
+ntx.stroke();
+
+const o = document.getElementById("myCanvas13");
+const otx = o.getContext("2d");
+
+otx.beginPath();
+otx.moveTo(20, 20);
+otx.bezierCurveTo(110, 150, 180, 10, 210, 140);
+otx.stroke();
