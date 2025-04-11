077fc1c194d11c4790b2869191cffe58b149ded4
snyderthing <gmsladen@gmail.com>
2025-04-11 15:57:23 -0700

    Tutorial 11 Case Problem 3 up to 7.f.

--- ParkCityGazette/pc_cword.js
+++ ParkCityGazette/pc_cword.js
@@ -71,38 +71,71 @@ function init() {
    let acrossID = currentLetter.dataset.clueA;
    let downID = currentLetter.dataset.clueD;
 
-// Set the value of acrossClue to reference the element with the id attribute "acrossID". 
-// Set the value of downClue to reference the element with the id attribute "downID".
+   // Set the value of acrossClue to reference the element with the id attribute "acrossID". 
+   // Set the value of downClue to reference the element with the id attribute "downID".
    acrossClue = acrossID;
    downClue = downID;
 }
 
-/*
-  document.onkeydown = selectLetter;
 
-  document.getElementById("directionImg").onclick = switchTypeDirection;
-  document.getElementById("showErrors").onclick = function () {
-    for (var i = 0; i < allLetters.length; i++) {
+document.onkeydown = selectLetter;
+
+document.getElementById("directionImg").onclick = switchTypeDirection;
+
+document.getElementById("showErrors").onclick = function () {
+   for (var i = 0; i < allLetters.length; i++) {
       if (allLetters[i].textContent !== allLetters[i].dataset.letter) {
-        allLetters[i].style.color = "red";
+         allLetters[i].style.color = "red";
       }
-    }
-    setTimeout(function () {
+   }
+   setTimeout(function () {
       for (var i = 0; i < allLetters.length; i++) {
-        allLetters[i].style.color = "";
+         allLetters[i].style.color = "";
       }
-    }, 3000);
-  };
-  document.getElementById("showSolution").onclick = function () {
-    for (var i = 0; i < allLetters.length; i++) {
-      allLetters[i].textContent = allLetters[i].dataset.letter;
-    }
-  };
-*/
+   }, 3000);
+};
 
+document.getElementById("showSolution").onclick = function () {
+   for (var i = 0; i < allLetters.length; i++) {
+      allLetters[i].textContent = allLetters[i].dataset.letter;
+   }
+};
+
+
+function formatPuzzle(puzzleLetter) {
+   currentLetter = puzzleLetter;
+   for (let i = 0; i < allLetters.length; i++) {
+      allLetters[i].style.backgroundColor = "";
+   }
+   acrossClue.style.color = "";
+   downClue.style.color = "";
+   if (currentLetter.dataset.clueA != undefined) {
+      acrossClue = document.getElementById(currentLetter.dataset.clueA);
+      acrossClue.style.color = "blue";
+      wordLetters = document.querySelectorAll("[data-clue-A = " + currentLetter.dataset.clueA + "]");
+      for (let i = 0; i < wordLetters.length; i++) {
+         wordLetters[i].style.backgroundColor = "rgb(231, 231, 255)";
+      }
+   }
+   
+   if (currentLetter.dataset.clueD != undefined) {
+      downClue = document.getElementById(currentLetter.dataset.clueD);
+      downClue.style.color = "red";
+      wordLetters = document.querySelectorAll("[data-clue-D = " + currentLetter.dataset.clueD + "]");
+      for (let i = 0; i < wordLetters.length; i++) {
+         wordLetters[i].style.backgroundColor = "rgb(255, 231, 231)";
+      }
+   }
+
+   if (typeDirection === "right") {
+      currentLetter.style.backgroundColor = "rgb(191, 191, 255";
+   } else {
+      currentLetter.style.backgroundColor = "rgb(255, 191, 191)";
+   }
+}
 
 /*====================================================*/
 
 function getChar(keyNum) {
-  return String.fromCharCode(keyNum);
+   return String.fromCharCode(keyNum);
 }
