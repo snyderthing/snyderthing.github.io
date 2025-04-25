fb9718ad571102ba3479e9c07ca2abaa085ee168
snyderthing <gmsladen@gmail.com>
2025-04-24 20:41:43 -0700

    Added Artster with figlet

--- Node/Jokester/index.js
+++ Node/Jokester/index.js
@@ -1,5 +1,13 @@
 const jokes = require("give-me-a-joke");
 const colors = require("colors");
+const cowsay = require("cowsay");
+
 jokes.getRandomDadJoke(function (joke) {
     console.log(joke.rainbow);
 });
+
+console.log(cowsay.say({
+    text: "I'm a moooooooodule",
+    e: "oO",
+    T: "U "
+}));
