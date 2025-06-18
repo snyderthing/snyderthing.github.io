a92d63d83d3ba0518b4584e73e466c174d7077c9
snyderthing <gmsladen@gmail.com>
2025-06-17 23:38:09 -0700

    Finished Tutorial 12 Review Assignment

--- Constitution/bc_keys.js
+++ Constitution/bc_keys.js
@@ -46,7 +46,6 @@ function makeKeyStyles() {
    alert("event2");
 }
 */
-
 window.addEventListener("load", findKeyWords);
 window.addEventListener("load", makeKeyStyles);
 
@@ -82,11 +81,103 @@ function findKeyWords() {
       8. Sort the keyWords array in alphabetical order.
    */
    keyWords.sort();
+   
+   for (var i = 0; i < keyWords.length; i++) {
+      var keyWordListItem = document.createElement("li");
+      var keyWordLink = document.createElement("a");
+      keyWordLink.innerHTML = keyWords[i].toString(); // this is an array, so maybe "textContent" isn't right.
+      var linkID = replaceWS(keyWords[i]);
+      keyWordLink.setAttribute("href", "#keyword_" + linkID); // two arguments needed
+      keyWordListItem.appendChild(keyWordLink);
+      outlineList.appendChild(keyWordListItem);
+   }
+   /*
+      9. Next, generate the list items in the keyword list. Add a for loop that loops through each item in
+      the keyWords array, doing the following for each item:
+         a. Declare the keyWordListItem variable, storing an li element.
+         b. Declare the keyWordLink variable storing an a element.
+         c. Change the innerHTML of keyWordLink to the value of the text of the current keyword.
+         d. Declare the linkID variable containing the value returned by the replaceWS() function using the current keyword as the parameter value.
+         e. Change the href attribute of keyWordLink to "#keyword_linkID" where linkID is the value of the linkID variable.
+         f. Append keyWordLink to keyWordList and then append keyWordList to the ordered list you created in step 6.
+   */
 }
 
 function makeKeyStyles() {
-   console.log("makeKeyStyles");
+   // Append an embedded style sheet to the document head
+   var keywordsBoxStyles = document.createElement("style");
+   document.head.appendChild(keywordsBoxStyles);
+
+   // Add style rules to the embedded style sheet
+   document.styleSheets[document.styleSheets.length - 1].insertRule(
+      "aside#keywords { \
+            border: 3px solid rgb(101, 101, 101); \
+            float: right; \
+            margin: 20px 0px 20px 20px; \
+            padding: 10px; \
+            width: 320px; \
+         }", 0);
+   
+   document.styleSheets[document.styleSheets.length - 1].insertRule(
+     "aside#keywords h1 { \
+            font-size: 2em; \
+            margin: 5px; \
+            text-align: center; \
+         }", 1);
+   document.styleSheets[document.styleSheets.length - 1].insertRule(
+      "aside#keywords ol { \
+            margin-left: 20px; \
+            font-size: 1.2em; \
+         }", 2);
+   
+   document.styleSheets[document.styleSheets.length - 1].insertRule(
+      "aside#keywords ol li { \
+            line-height: 1.5em; \
+         }", 2);
+   
+   document.styleSheets[document.styleSheets.length - 1].insertRule(
+      "aside#keywords ol li a { \
+            color: rgb(101, 101, 101); \
+            text-decoration: none; \
+         }", 3);
 }
+   /*
+      11. Create the makeKeyStyles() function that will define the
+          style sheet for the keywords box. Add the following 
+          commands to the function:
+          a. Create an embedded style sheet and append it to the document head.
+          b. Add the following style rules to the style sheet:
+
+          aside#keywords {
+            border: 3px solid rgb(101, 101, 101);
+            float: right;
+            margin: 20px 0px 20px 20px;
+            padding: 10px;
+            width: 320px;
+         }
+         
+         aside#keywords h1 {
+            font-size: 2em;
+            margin: 5px;
+            text-align: center;
+         }
+         
+         aside#keywords ol {
+            margin-left: 20px;
+            font-size: 1.2em;
+         }
+         
+         aside#keywords ol li {
+            line-height: 1.5em;
+         }
+         
+         aside#keywords ol li a {
+            color: rgb(101, 101, 101);
+            text-decoration: none;
+         }
+   */
+
+
 
 
 
