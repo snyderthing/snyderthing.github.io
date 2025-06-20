447f33596b4551a97ae424de2381fe1b508dd27c
snyderthing <gmsladen@gmail.com>
2025-06-19 21:07:41 -0700

    Finished Case problem 1 of Tutorial 12

--- NewAccents/na_styler.js
+++ NewAccents/na_styler.js
@@ -51,9 +51,9 @@ function setStyles() {
    divBox.appendChild(figBox);
 
    /*
-      Next, populate the figure box with preview images of the five fancy style sheets. 
-      Insert a for loop with an index that goes from 0 up through 4 and each time through
-      the loop do the following:
+      8. Next, populate the figure box with preview images of the five fancy style sheets. 
+         Insert a for loop with an index that goes from 0 up through 4 and each time through
+         the loop do the following:
          a. Create an img element node named sheetImg with a src attribute of "na_small_num.png"
             and an alt attribute value of "na_style_num.css" where num is the value of the index 
             in the for loop.
@@ -61,15 +61,59 @@ function setStyles() {
             thumbnail images by adding an event handler to sheetImg that runs an anonymous
             function changing the href attribute of the link element with the ID "fancySheet"
             to the value of the alt attribute of the event target.
-         c. Append sheetImg to the figBox element nod.
+         c. Append sheetImg to the figBox element node.
    */
    for (var i = 0; i < 5; i++) {
       var sheetImg = document.createElement("img");
       sheetImg.setAttribute("src", "na_small_" + i + ".png");
-      sheetImg.setAttribute("alt", "na_small_" + i + ".css");
-      sheetImg.addEventListener("onsomething")
-
+      sheetImg.setAttribute("alt", "na_style_" + i + ".css");
+      sheetImg.onclick = function (e) {
+         document.getElementById("fancySheet").href = e.target.alt;
+      };
+      figBox.appendChild(sheetImg);
    }
+   /*
+      9. Next, design the appearance of the thumbnail figure box. Create an embedded
+         style sheet named thumbStyles and append it to the document head.
+   */
+   var thumbStyles = document.createElement("style");
+   document.head.appendChild(thumbStyles);
+   /*
+      10. Add the following style rules to the thumbStyles style sheet:
+
+         figure#styleThumbs {
+            position: absolute;
+            left: 0px;
+            bottom: 0px;
+         }
+         
+         figure#styleThumbs img {
+            outline: 1px solid black;
+            cursor: pointer;
+            opacity: 0.75;
+         }
+         
+         figure#styleThumbs img:hover {
+            outline: 1px solid red;
+            opacity: 1.0;
+         }
+   */
+   document.styleSheets[document.styleSheets.length - 1].insertRule(
+      "figure#styleThumbs { \
+            position: absolute; \
+            left: 0px; \
+            bottom: 0px;}", 0);
+   
+   document.styleSheets[document.styleSheets.length - 1].insertRule(
+      "figure#styleThumbs img { \
+            outline: 1px solid black; \
+            cursor: pointer; \
+            opacity: 0.75;}", 1);
+   
+   document.styleSheets[document.styleSheets.length - 1].insertRule(
+      "figure#styleThumbs img:hover { \
+            outline: 1px solid red; \
+            opacity: 1.0;}", 2);
 }
 
 function randInt(size) {
