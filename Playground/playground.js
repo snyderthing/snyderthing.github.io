557dc42fc932eaa6e1a054822dc0d96bbb0f0c59
snyderthing <107716018+snyderthing@users.noreply.github.com>
2026-06-27 22:01:48 -0700

    Perform minor cleanup

--- Playground/playground.js
+++ Playground/playground.js
@@ -4,3 +4,5 @@ const image = document.getElementById("color-palette");
 container.addEventListener("click", function () {
     image.classList.toggle("hidden");
 });
+
+console.log("Playground script loaded successfully.");
