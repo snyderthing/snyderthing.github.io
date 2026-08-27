e0de33d127705d918ddf69361a7ff39851a16506
snyderthing <107716018+snyderthing@users.noreply.github.com>
2026-08-27 08:32:40 -0700

    Do Tutorial 3, Review Assignment

--- BasicCalculator/script.js
+++ BasicCalculator/script.js
@@ -1,11 +1,35 @@
+const input = document.getElementById("inputBox");
+const container = document.getElementById("container");
+
+container.addEventListener("click", function (e) {
+    buttonClick(e.target.id);
+});
+const calculateButton = document.getElementById("button=");
+calculateButton.addEventListener("click", calculate);
+
+const clearButton = document.getElementById("buttonc");
+clearButton.addEventListener("click", clearDisplay);
+
+function buttonClick(buttonId) {
+    if (buttonId !== "buttonc" && buttonId !== "button=") {
+        const s = buttonId.replace("button", "");
+        addToDisplay(s);
+    }
+}
+
+function addToDisplay(value) {
+    input.value += value;
+}
+
 function calculate() {
-    document.calc.output.value = eval(document.calc.output.value);
+    if (input.value === "" || input.value === ".") {
+        alert("Please enter a mathematical expression");
+        return;
+    }
+
+    input.value = eval(input.value);
 }
 
-function erase() {
-    document.calc.output.value = '';
-}
-
-function entries(s) {
-    document.calc.output.value+=s;
+function clearDisplay() {
+    input.value = '';
 }
