65d8a2177f6c3e97adb5f94dd073bf6bf8c31309
snyderthing <gmsladen@gmail.com>
2026-09-07 10:50:48 -0700

    Update Colt Steele templating files with prettier footer and kind of dynamic page title

--- ColtSteele/BootcampSection34/Bootstrap_Demo/index.js
+++ ColtSteele/BootcampSection34/Bootstrap_Demo/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const redditData = require('./data.json');
+const { name } = require('ejs');
 
 app.use(express.static(path.join(__dirname, 'public')))
 
@@ -9,29 +10,29 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
 
 app.get('/', (req, res) => {
-    res.render('home')
+    res.render('home', {title: 'Home'})
 })
 
 app.get('/cats', (req, res) => {
     const cats = [
         'Blue', 'Rocket', 'Monty', 'Stephanie', 'Winston'
     ]
-    res.render('cats', { cats })
+    res.render('cats', { cats, title: 'cats' })
 })
 
 app.get('/r/:subreddit', (req, res) => {
     const { subreddit } = req.params;
     const data = redditData[subreddit];
     if (data) {
-        res.render('subreddit', { ...data });
+        res.render('subreddit', { ...data, title: 'subreddit' }); // This title could be dynamic to match the param... if you knew how....
     } else {
-        res.render('notfound', { subreddit })
+        res.render('notfound', { subreddit, title: 'Not Found' })
     }
 })
 
 app.get('/rand', (req, res) => {
     const num = Math.floor(Math.random() * 10) + 1;
-    res.render('random', { num })
+    res.render('random', { num, title: 'random' })
 })
 
 app.listen(3000, () => {
