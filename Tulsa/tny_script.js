c3cd4669dabd39c797077b1e4667f0bee1c1914c
snyderthing <107716018+snyderthing@users.noreply.github.com>
2026-09-22 12:56:39 -0700

    Do Tutorial 9, Session 2

--- Tulsa/tny_script.js
+++ Tulsa/tny_script.js
@@ -11,3 +11,16 @@
 
 */
 
+/* Store the current date and time */
+const currentDay = new Date("May 23, 2018 14:35:05");
+const dateStr = currentDay.toLocaleDateString();
+const timeStr = currentDay.toLocaleTimeString();
+
+/* Display the current date and time */
+document.getElementById("dateNow").innerHTML = dateStr + "<br />" + timeStr;
+
+/* Display the time left until New Year's Eve */
+document.getElementById("days").textContent = "dd";
+document.getElementById("hrs").textContent = "hh";
+document.getElementById("mins").textContent = "mm";
+document.getElementById("secs").textContent = "ss";
